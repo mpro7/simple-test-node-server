ea3b3d57eff214134a0aa3a7dabe9660af4f4807
mpro7 <justinstarlight@gmail.com>
2019-03-15 22:52:44 +0100

    added post method + updated form processing

--- server.js
+++ server.js
@@ -1,38 +1,69 @@
 var http = require('http');
-var formidable = require('formidable');
+var formidable = require("formidable");
 var util = require('util');
 
 var server = http.createServer(function (req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requeste-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
-  if (req.method.toLowerCase() == 'post') {
-    processForm(req, res);
-    return;
-  }
+    if (req.method.toLowerCase() == 'post') {
+        processForm(req, res);
+        return;
+    }
 
-  res.end();
+    if (req.method.toLowerCase() == 'get') {
+        var data = {
+            data: {
+                languages: [
+                    'English',
+                    'Spanish',
+                    'German',
+                    'Other'
+                ]
+            }
+        };
+
+        var responseData = JSON.stringify(data);
+        res.end(responseData);
+        console.log("get: ", responseData);
+        return;
+    }
+
+    res.end();
 });
 
+
 function processForm(req, res) {
-  var form = new formidable.IncomingForm();
+    var form = new formidable.IncomingForm();
 
-  form.parse(req, function (err, fields) {
-    res.writeHead(200, {
-      'content-type': 'text/plain'
+    form.parse(req, function (err, fields) {
+
+        fields.id = 'ABC123';
+
+        res.writeHead(400, {
+            'content-type': 'text/plain'
+        });
+
+        // res.end(util.inspect({
+        //   fields: fields
+        // }));
+    
+        // console.log('posted fields:\n');
+        // console.log(util.inspect({
+        //   fields: fields
+        // }));
+        
+        var data = JSON.stringify({
+            fields: fields
+        });
+
+        res.end(data);
+
+        console.log("posted fields:");
+        console.log(data);
     });
-
-    res.end(util.inspect({
-      fields: fields
-    }));
-
-    console.log('posted fields:\n');
-    console.log(util.inspect({
-      fields: fields
-    }));
-  });
 }
 
-var port = 3000;
+var port = 3100;
 server.listen(port);
-console.log('Server listening on port ' + port);
+console.log("server listening on port " + port);
